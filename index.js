f24f8b9ae09179266e7fd35143892e084a6dd455
Suraj_Jadhav <surajjadhav029@gmail.com>
2024-05-30 13:58:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}! This is 2024`);
+  res.send(`Hello ${name}! version 1`);
 });
 
 app.listen(port, () => {
